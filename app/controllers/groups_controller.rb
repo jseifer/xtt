82743212899d530ade553a488bb7997c97f09464
Matt Lyon <matt@ameratsu.local>
2008-01-09 14:11:43 -0800

    regen groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,51 +1,80 @@
 class GroupsController < ApplicationController
-  before_filter :find_group, :only => [:show, :edit, :update, :destroy]
-  before_filter :login_required
-
   def index
     @groups = Group.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml  => @groups }
+      format.json { render :json => @groups }
+    end
   end
 
   def show
+    @group = Group.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml  => @group }
+      format.json { render :json => @group }
+    end
   end
 
   def new
     @group = Group.new
-  end
 
-  def create
-    @group = Group.new(params[:group])
-    if @group.save
-      flash[:notice] = 'Group was successfully created.'
-      redirect_to @group
-    else
-      render :action => 'new'
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml  => @group }
+      format.json { render :json => @group }
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(params[:group])
+
+    respond_to do |format|
+      if @group.save
+        flash[:notice] = 'Group was successfully created.'
+        format.html { redirect_to(@group) }
+        format.xml  { render :xml  => @group, :status => :created, :location => @group }
+        format.json { render :json => @group, :status => :created, :location => @group }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml  => @group.errors, :status => :unprocessable_entity }
+        format.json { render :json => @group.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    if @group.update_attributes(params[:group])
-      flash[:notice] = 'Group was successfully updated.'
-      redirect_to @group
-    else
-      render :action => 'edit'
+    @group = Group.find(params[:id])
+
+    respond_to do |format|
+      if @group.update_attributes(params[:group])
+        flash[:notice] = 'Group was successfully updated.'
+        format.html { redirect_to(@group) }
+        format.xml  { head :ok }
+        format.json { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml  => @group.errors, :status => :unprocessable_entity }
+        format.json { render :json => @group.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    @group.destroy
-    redirect_to groups_path
-  end
-
-protected
-  def find_group
     @group = Group.find(params[:id])
-  end
-  
-  def authorized?
-    logged_in? && (admin? || @group.nil? || @group.users.include?(current_user))
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(groups_url) }
+      format.xml  { head :ok }
+      format.json { head :ok }
+    end
   end
 end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,41 +1,58 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe GroupsController, "GET #index" do
-  define_models
+  # fixture definition
 
   act! { get :index }
 
   before do
     @groups = []
     Group.stub!(:find).with(:all).and_return(@groups)
-    controller.stub!(:login_required)
   end
   
-  it_assigns :groups
-  it_renders :template, :index
+  it.assigns :groups
+  it.renders :template, :index
+
+  describe GroupsController, "(xml)" do
+    # fixture definition
+    
+    act! { get :index, :format => 'xml' }
+
+    it.assigns :groups
+    it.renders :xml, :groups
+  end
+
 end
 
 describe GroupsController, "GET #show" do
-  define_models
+  # fixture definition
 
   act! { get :show, :id => 1 }
 
   before do
     @group  = groups(:default)
     Group.stub!(:find).with('1').and_return(@group)
-    controller.stub!(:login_required)
   end
   
-  it_assigns :group
-  it_renders :template, :show
+  it.assigns :group
+  it.renders :template, :show
+  
+  describe GroupsController, "(xml)" do
+    # fixture definition
+    
+    act! { get :show, :id => 1, :format => 'xml' }
+
+    it.renders :xml, :group
+  end
+
+
 end
 
 describe GroupsController, "GET #new" do
-  define_models
+  # fixture definition
   act! { get :new }
   before do
     @group  = Group.new
-    controller.stub!(:login_required)
   end
 
   it "assigns @group" do
@@ -43,21 +60,29 @@ describe GroupsController, "GET #new" do
     assigns[:group].should be_new_record
   end
   
-  it_renders :template, :new
+  it.renders :template, :new
+  
+  describe GroupsController, "(xml)" do
+    # fixture definition
+    act! { get :new, :format => 'xml' }
+
+    it.renders :xml, :group
+  end
+
+
 end
 
 describe GroupsController, "GET #edit" do
-  define_models
+  # fixture definition
   act! { get :edit, :id => 1 }
   
   before do
     @group  = groups(:default)
     Group.stub!(:find).with('1').and_return(@group)
-    controller.stub!(:login_required)
   end
 
-  it_assigns :group
-  it_renders :template, :edit
+  it.assigns :group
+  it.renders :template, :edit
 end
 
 describe GroupsController, "POST #create" do
@@ -65,34 +90,57 @@ describe GroupsController, "POST #create" do
     @attributes = {}
     @group = mock_model Group, :new_record? => false, :errors => []
     Group.stub!(:new).with(@attributes).and_return(@group)
-    controller.stub!(:login_required)
   end
   
   describe GroupsController, "(successful creation)" do
-    define_models
+    # fixture definition
     act! { post :create, :group => @attributes }
 
     before do
       @group.stub!(:save).and_return(true)
-      controller.stub!(:login_required)
     end
     
-    it_assigns :group, :flash => { :notice => :not_nil }
-    it_redirects_to { group_path(@group) }
+    it.assigns :group, :flash => { :notice => :not_nil }
+    it.redirects_to { group_path(@group) }
   end
 
   describe GroupsController, "(unsuccessful creation)" do
-    define_models
+    # fixture definition
     act! { post :create, :group => @attributes }
 
     before do
       @group.stub!(:save).and_return(false)
-      controller.stub!(:login_required)
     end
     
-    it_assigns :group
-    it_renders :template, :new
+    it.assigns :group
+    it.renders :template, :new
   end
+  
+  describe GroupsController, "(successful creation, xml)" do
+    # fixture definition
+    act! { post :create, :group => @attributes, :format => 'xml' }
+
+    before do
+      @group.stub!(:save).and_return(true)
+      @group.stub!(:to_xml).and_return("mocked content")
+    end
+    
+    it.assigns :group, :headers => { :Location => lambda { group_url(@group) } }
+    it.renders :xml, :group, :status => :created
+  end
+  
+  describe GroupsController, "(unsuccessful creation, xml)" do
+    # fixture definition
+    act! { post :create, :group => @attributes, :format => 'xml' }
+
+    before do
+      @group.stub!(:save).and_return(false)
+    end
+    
+    it.assigns :group
+    it.renders :xml, "group.errors", :status => :unprocessable_entity
+  end
+
 end
 
 describe GroupsController, "PUT #update" do
@@ -100,47 +148,77 @@ describe GroupsController, "PUT #update" do
     @attributes = {}
     @group = groups(:default)
     Group.stub!(:find).with('1').and_return(@group)
-    controller.stub!(:login_required)
   end
   
   describe GroupsController, "(successful save)" do
-    define_models
+    # fixture definition
     act! { put :update, :id => 1, :group => @attributes }
 
     before do
       @group.stub!(:save).and_return(true)
-      controller.stub!(:login_required)
     end
     
-    it_assigns :group, :flash => { :notice => :not_nil }
-    it_redirects_to { group_path(@group) }
+    it.assigns :group, :flash => { :notice => :not_nil }
+    it.redirects_to { group_path(@group) }
   end
 
   describe GroupsController, "(unsuccessful save)" do
-    define_models
+    # fixture definition
     act! { put :update, :id => 1, :group => @attributes }
 
     before do
       @group.stub!(:save).and_return(false)
-      controller.stub!(:login_required)
     end
     
-    it_assigns :group
-    it_renders :template, :edit
+    it.assigns :group
+    it.renders :template, :edit
   end
+  
+  describe GroupsController, "(successful save, xml)" do
+    # fixture definition
+    act! { put :update, :id => 1, :group => @attributes, :format => 'xml' }
+
+    before do
+      @group.stub!(:save).and_return(true)
+    end
+    
+    it.assigns :group
+    it.renders :blank
+  end
+  
+  describe GroupsController, "(unsuccessful save, xml)" do
+    # fixture definition
+    act! { put :update, :id => 1, :group => @attributes, :format => 'xml' }
+
+    before do
+      @group.stub!(:save).and_return(false)
+    end
+    
+    it.assigns :group
+    it.renders :xml, "group.errors", :status => :unprocessable_entity
+  end
+
 end
 
 describe GroupsController, "DELETE #destroy" do
-  define_models
+  # fixture definition
   act! { delete :destroy, :id => 1 }
   
   before do
     @group = groups(:default)
     @group.stub!(:destroy)
     Group.stub!(:find).with('1').and_return(@group)
-    controller.stub!(:login_required)
   end
 
-  it_assigns :group
-  it_redirects_to { groups_path }
+  it.assigns :group
+  it.redirects_to { groups_path }
+  
+  describe GroupsController, "(xml)" do
+    # fixture definition
+    act! { delete :destroy, :id => 1, :format => 'xml' }
+
+    it.assigns :group
+    it.renders :blank
+  end
+
 end
