b9efc955a758c338cf14e45e8ed36440bdb52dc1
rick <technoweenie@gmail.com>
2008-01-08 17:41:32 -0800

    lots of controller access specs with lots of failures

---
+++ spec/controllers/projects_controller_access_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe_access_for ProjectsController do
+  before { define_models :users }
+
+  as :default, :admin do
+    it_allows :get, [:index, :new]
+    it_allows :post, :create
+    it_allows :put,    :update, :id => 1
+    it_allows :delete, :destroy, :id => 1
+  end
+  
+  as :anon, :pending, :suspended do
+    it_restricts :get, [:index, :new]
+    it_restricts :post, :create
+    it_restricts :put,    :update, :id => 1
+    it_restricts :delete, :destroy, :id => 1
+  end
+end
--- spec/controllers/users_controller_access_spec.rb
+++ spec/controllers/users_controller_access_spec.rb
@@ -4,13 +4,39 @@ describe_access_for UsersController do
   before { define_models :users }
 
   as :anon, :default do
-    it_allows :post, :create
     it_allows :get, [:index, :new]
+    it_allows :post, :create
     it_restricts :put,    [:suspend, :unsuspend], :id => 1
     it_restricts :delete, [:destroy, :purge], :id => 1
+    it_performs "activates with", :get, :activate, :key => 'foo' do
+      response.should redirect_to(root_path)
+    end
   end
   
   as :anon do
     it_restricts :get, [:show, :edit], :id => 1
+  end
+  
+  as :pending do
+    it_performs "activates with", :get, :activate, :key => 'foo' do
+      response.should redirect_to(root_path)
+    end
+  end
+  
+  as :pending, :suspended do
+    it_restricts :get, [:index, :new]
+    it_restricts :post, :create
+    it_restricts :put,    [:suspend, :unsuspend], :id => 1
+    it_restricts :delete, [:destroy, :purge], :id => 1
+  end
+  
+  as :admin do
+    it_allows :get, [:index, :new]
+    it_allows :post, :create
+    it_allows :put,    [:suspend, :unsuspend], :id => 1
+    it_allows :delete, [:destroy, :purge], :id => 1
+    it_performs "activates with", :get, :activate, :key => 'foo' do
+      response.should redirect_to(root_path)
+    end
   end
 end
