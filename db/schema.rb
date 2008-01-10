abe1fc22812ea05fb84224c0423047b1bd305d5f
Matt Lyon <matt@ameratsu.local>
2008-01-09 18:54:50 -0800

    fucking schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 10) do
+ActiveRecord::Schema.define(:version => 11) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(:version => 10) do
   end
 
   create_table "memberships", :force => true do |t|
-    t.integer "group_id"
-    t.integer "user_id"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.string   "state",      :default => "passive"
+    t.datetime "deleted_at"
   end
 
   create_table "projects", :force => true do |t|
