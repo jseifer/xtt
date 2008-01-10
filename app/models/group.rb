58c7eb8b35309d959e8a28caae37e1f405aa7744
Matt Lyon <matt@ameratsu.local>
2008-01-09 18:41:01 -0800

    group owners dont need to be faked in as members since we add them for realz

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,9 +7,6 @@ class Group < ActiveRecord::Base
   belongs_to :owner, :class_name => User.name
   after_create Proc.new {|g| g.memberships.create(:user => g.owner) }
   
-  has_many :users, :order => 'login', :through => :memberships do
-    def include?(user)
-      proxy_owner.owner_id == user.id || (loaded? ? @target.include?(user) : exists?(user.id))
-    end
-  end
+  has_many :users, :order => 'login', :through => :memberships
+  
 end
