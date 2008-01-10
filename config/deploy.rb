425d04fa52ed86560acee9af97e8d7ed7b5d253b
courtenay <court3nay@gmail.com>
2008-01-09 23:57:44 -0800

    symlink after demploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "micromanage"
-set :repository,  "git@activereload.net:lh-time.git"
+#xset :repository,  "git@activereload.net:lh-time.git"
 set :deploy_to, "/var/rails/#{application}"
-set :scm, :git
+# set :scm, :git
 
 set :deploy_via, "copy"
 
@@ -9,8 +9,17 @@ role :app, "two"
 role :web, "two"
 role :db,  "two", :primary => true
 
+task :after_update_code, :roles => :app do
+  put(File.read('config/database.yml'), "#{release_path}/config/database.yml", :mode => 0444) 
+  run <<-CMD
+    cd #{release_path} && rake tmp:create
+  CMD
+  run "ln -s #{shared_path}/mongrel_cluster.yml #{release_path}/config/"
+end
+
 namespace :deploy do
   task :restart do
+    run "cd #{release_path} && mongrel_rails cluster::restart"
   end
 
   task :start do
