09a6a721cacd839bd08b6148346ad42f746bc617
Justin Palmer <justin@activereload.net>
2008-01-15 14:19:35 -0800

    Updated the projects view, abstracted the special box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,24 +4,12 @@ require 'md5'
 
 module ApplicationHelper
 
-  def sidebar_box(name, &block)
-    sbox = OpenStruct.new
-    sbox.name= name
-    yield sbox
-    render :file => '/components/sidebar_box', :locals => {:sbox => sbox}
-  end
-  
-  # Doesn't work.  Requries a Level 6 in Rails Voodoo. 
-  def main_box(name, &block)
-    mbox = ContentBox.new(name)
-    yield mbox
-    render :file => '/components/main_box', :locals => {:mbox => mbox}
-  end
-  
-  def fullscreen_box(name, &block)
-    fbox = FullScreenBox.new(name)
-    yield fbox
-    render :file => '/components/fullscreen_box', :locals => {:fbox => fbox}
+  def box(type, name, &block)
+    type = type.to_s
+    box = OpenStruct.new
+    box.name= name
+    yield box
+    render :file => "/components/#{type}_box", :locals => {:box => box}
   end
   
   def gravatar_for(user)
