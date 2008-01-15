4a6b6908816d3019ae87223a35107f9efe7db36b
rick <technoweenie@gmail.com>
2008-01-15 12:09:44 -0800

    basic memberships stuff

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -2,4 +2,18 @@ module StatusesHelper
   def status_summary(project)
     project.nil? ? "is out" : "is working on #{link_to_project project}"
   end
+  
+  def status_for(user_or_status)
+    message = user_or_status.is_a?(User) ? user_or_status.last_status_message : user_or_status.message
+    message.blank? ? "No update" : h(message)
+  end
+  
+  def status_at(status_or_date)
+    created_at = case status_or_date
+      when User   then status_or_date.last_status_at
+      when Status then status_or_date.created_at
+      else status_or_date
+    end
+    created_at && "#{time_ago_in_words created_at} ago"
+  end
 end
