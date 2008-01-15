8aabed38759e1b5a3e44b3c1344ff4cd91db3807
Justin Palmer <justin@activereload.net>
2008-01-15 00:18:34 -0800

    fix migration 12

--- db/migrate/012_rename_project_parent_to_user.rb
+++ db/migrate/012_rename_project_parent_to_user.rb
@@ -4,6 +4,7 @@ class RenameProjectParentToUser < ActiveRecord::Migration
 
   def self.up
     Project.update_all ['parent_id = ?', 1], ['parent_type = ?', 'Group']
+    remove_index  :projects, :name => 'index_projects_on_name_and_parent'
     remove_column :projects, :parent_type
     rename_column :projects, :parent_id, :user_id
   end
