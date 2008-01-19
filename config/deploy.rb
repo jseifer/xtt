9e1f0b538f7fc6321109c2621672b291b1c42125
courtenay <court3nay@gmail.com>
2008-01-18 18:32:21 -0800

    fix deploy, add backup task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "micromanage"
-#xset :repository,  "git@activereload.net:lh-time.git"
+set :repository,  "git@activereload.net:lh-time.git"
 set :deploy_to, "/var/rails/#{application}"
-# set :scm, :git
+set :scm, :git
 
 set :deploy_via, "copy"
 
@@ -27,4 +27,27 @@ namespace :deploy do
 
   task :stop do
   end
+end
+
+desc "Backup (mysqldump) production database and rsync to local box"
+task :backup, :roles => :db, :only => { :primary => true } do
+  # The on_rollback handler is only executed if this task is executed within
+  # a transaction (see below), AND it or a subsequent task fails.
+  filename = "/tmp/#{application}.dump.#{Time.now.to_f}.sql.bz2"
+  yaml = YAML::load_file('config/database.yml')
+  text = capture "cat #{current_path}/config/database.yml"
+  yaml = YAML::load(text)
+  conf = yaml['production']
+  
+  on_rollback { delete filename }
+
+  run "mysqldump -u #{conf['username']} -p -h #{conf['host'] || '127.0.0.1'} #{conf['database']} --ignore-table=#{conf['database']}.sessions | bzip2 -c > #{filename}" do |ch, stream, out|
+    ch.send_data "#{conf['password']}\n" if out =~ /^Enter password:/
+  end
+
+  `mkdir -p #{File.dirname(__FILE__)}/../backups`
+  `rsync #{roles[:db][0].host}:#{filename} #{File.dirname(__FILE__)}/../backups/`
+  run "rm -f #{filename}"
+  
+  backup_public_dir
 end
