a2be6d19b90ce532ce2758d7454975500b206ae2
Justin Palmer <justin@activereload.net>
2008-01-28 17:31:21 -0800

    Swap tzoffset stuff to JavaScript cookies

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,62 +2,18 @@ document.observe('dom:loaded', function() {
   // Make inputs with the class name hintable use their title attribute
   // for a hint.
   $$('input.hintable').invoke('hintable');
-  
+});
+
+
+(function() {
   // Get users Timezone offset, add it as a hidden field and 
   // submit it with every form
   var date = new Date();
   var offset = date.getTimezoneOffset();
-  $$('form').each(function(form) {
-    var input = new Element('input', {type:'hidden', name:'tzoffset', class:'tzoffset'});
-    form.insert(input);
-  });
-  $$('input.tzoffset').invoke('setValue', offset);
-  
-});
+  Cookie.set({tzoffset: offset});
+})();
 
-// var XTT = {
-//   adjust_utc: function() {
-//     $$('input.utc').each(function(input){ 
-//       input.setValue(new DateTime($F(input)).toLocalString());
-//     });
-//     $$('span.utc').each(function(i){
-//       i.innerHTML = new Date(Date.parse(i.innerHTML)) 
-//     });
-//   },
-// 
-//   nice_time: function(seconds) {
-//     /* live timer */
-//     var hours = (seconds / 3600).floor();
-//     seconds = seconds % 3600;
-//     var minutes = (seconds / 60).floor().toPaddedString(2);
-//     seconds = (seconds % 60).toPaddedString(2);
-//     return([hours, minutes, seconds].join(":"));
-//   },
-//   
-//   timerIncrement: function(dom) {
-//     dom = $(dom);
-//     var seconds = parseInt(dom.innerHTML);
-//     var d = new Date();
-//     var epoch = (d.getTime() - d.getMilliseconds()) / 1000;
-// 
-//     dom.nextSibling.innerHTML = XTT.nice_time(epoch - seconds);
-//   }
-// }
 
-// var DateTime = Class.create({
-//   initialize: function(date) {
-//     this.dateTime = "";
-//     if(Object.isString(date))
-//       this.dateTime = new Date(Date.parse(date));
-//     else if(date.constructor == Date) {
-//       this.dateTime = date;
-//     }
-//   },
-//   
-//   toLocalString: function() {
-//     return this.dateTime.toLocaleString();
-//   }
-// })
 
 Element.addMethods('INPUT', {
   /**
