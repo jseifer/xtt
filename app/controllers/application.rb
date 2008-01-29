6336b1def726c5e791667deb66cf6fe9ca7c9484
rick <technoweenie@gmail.com>
2008-01-29 15:15:12 -0800

    add user timezone

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
   before_filter :adjust_format_for_iphone
+  before_filter :set_timezone
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -20,5 +21,22 @@ protected
   # Set iPhone format if request to iphone.trawlr.com
   def adjust_format_for_iphone    
     request.format = :iphone if iphone_user_agent?
+  end
+  
+  # The browsers give the # of minutes that a local time needs to add to
+  # make it UTC, while TimeZone expects offsets in seconds to add to 
+  # a UTC to make it local.
+  def browser_timezone
+    return nil if params[:tzoffset].blank?
+    @browser_timezone ||= begin
+      min = params[:tzoffset].to_i
+      tz = TimeZone[(min + (-2 * min)).minutes]
+      current_user.update_attribute(:time_zone, tz.name) if tz && logged_in? && tz.name != current_user.time_zone
+      tz
+    end
+  end
+
+  def set_timezone
+    Time.zone = logged_in? ? current_user.time_zone : browser_timezone
   end
 end
