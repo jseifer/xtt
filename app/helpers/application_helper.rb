d09830466b72643ac691024c124ed3b19461d1e2
rick <technoweenie@gmail.com>
2008-02-06 18:11:08 -0800

    cleaner js time implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,20 +42,20 @@ module ApplicationHelper
   end
 
   @@default_jstime_format = "%d %b, %Y %I:%M %p"
-  def js_datetime(time, custom = nil)
-    content_tag('abbr', content_tag('span', time.utc.strftime(@@default_jstime_format), :class => "timestamp #{custom}".strip), :title => time.xmlschema, :class => 'published')
+  def js_datetime(time, rel = :datetime)
+    content_tag('abbr', content_tag('span', time.utc.strftime(@@default_jstime_format), :class => :timestamp, :rel => rel), :title => time.xmlschema, :class => 'published')
   end
   
   def js_time_ago_in_words(time)
-    js_datetime time, :formatted_words
+    js_datetime time, :words
   end
   
   def js_time(time)
-    js_datetime time, :formatted_time
+    js_datetime time, :time
   end
   
   def js_day(time)
-    js_datetime time, :formatted_day
+    js_datetime time, :day
   end
   
   def display_flash(key)
