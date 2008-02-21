899c62ac651a0e5fe804255447d24c4f27f8320d
Justin Palmer <justin@activereload.net>
2008-02-21 10:10:20 -0800

    Show exact date ranges or days in the project hour area

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,17 @@ module ProjectsHelper
   def link_to_project(project, text = nil, url_for_options = nil)
     link_to h(text || project.name), url_for_options || project
   end
+  
+  def period_to_sentence(filter, date_range, hours)
+    plural = hours == 1 ? 'hour' : 'hours'
+    final = case params[:filter]
+      when :daily
+        date_range.first.strftime("%A, %b %d")
+      when :monthly
+        "for #{date_range.first.strftime("%B")}"
+      when :'bi-weekly', :weekly
+        "#{date_range.first.strftime("%B %d")}-#{date_range.last.strftime("%d")}"
+    end
+    %(#{plural} <span class="daterange">#{final}</span>)
+  end
 end
