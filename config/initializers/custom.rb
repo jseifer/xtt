2553c2e44613a81d218435ab4818c2515423f31c
courtenay <court3nay@gmail.com>
2008-02-20 18:41:29 -0800

    fix urls

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -1,5 +1,5 @@
-TT_HOST  = 'localhost'
-TT_EMAIL = 'replace-me@tt.com'
+TT_HOST  = 'tt.entp.com'
+TT_EMAIL = 'courtenay+tt@entp.com'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update \
  :time => '%I:%M %p'
