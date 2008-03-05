5875e9e5f5a7f2f8dbfb60256cf8141fa3c78b1f
Court3nay <courtenay@caboo.se>
2008-03-05 11:18:30 -0800

    Basic openid support

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    self.current_user = User.authenticate(params[:login], params[:password])
-    if logged_in?
-      if params[:remember_me] == "1"
-        self.current_user.remember_me
-        cookies[:auth_token] = { :value => current_user.remember_token , :expires => current_user.remember_token_expires_at }
-      end
-      redirect_back_or_default
-      flash[:notice] = "Logged in successfully"
+    if using_open_id?
+      openid_auth(params[:openid_url])
     else
-      flash[:notice] = "Invalid login or password."
-      render :action => 'new'
+      password_auth(params[:login], params[:password])
     end
   end
 
@@ -27,4 +20,51 @@ class SessionsController < ApplicationController
     flash[:notice] = "You have been logged out."
     redirect_back_or_default
   end
+
+
+protected
+
+  def openid_auth(identity_url)
+    authenticate_with_open_id(identity_url) do |status, identity_url|
+      case status
+      when :missing
+        failed_login "Sorry, the OpenID server couldn't be found"
+      when :canceled
+        failed_login "OpenID verification was canceled"
+      when :failed
+        failed_login "Sorry, the OpenID verification failed"
+      when :successful
+        if self.current_user = @account.users.find_by_identity_url(identity_url)
+          successful_login "Welcome!"
+        else
+          failed_login "Sorry, no user by that identity URL exists"
+        end
+      end
+    end
+  end
+  
+  def password_auth(login, password)
+    self.current_user = User.authenticate(login, password)
+    if logged_in?
+      if params[:remember_me] == "1"
+        self.current_user.remember_me
+        cookies[:auth_token] = { :value => current_user.remember_token , :expires => current_user.remember_token_expires_at }
+      end
+      successful_login "Logged in successfully"
+    else
+      failed_login "Invalid login or password."
+    end
+  end
+  
+  def failed_login(message)
+    flash[:notice] = message
+    render :action => "new"
+  end
+  
+  def successful_login(message=nil)
+    flash[:notice] = message
+    redirect_back_or_default
+  end
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,8 @@
 class User < ActiveRecord::Base
-  concerned_with :authentication, :state_machine
-  
-  attr_readonly :last_status_project_id, :last_status_id, :last_status_message
+  concerned_with :authentication, :state_machine, :statuses
   
   before_create { |u| u.admin = true if User.count.zero? }
-  
-  belongs_to :last_status_project, :class_name => "Project"
-  belongs_to :last_status, :class_name => "Status"
-
-  has_many :statuses, :order => 'statuses.created_at desc', :extend => Status::Methods::AssociationExtension do
-    def filter(filter = :weekly, options = {})
-      Status.filter proxy_owner.id, filter, options
-    end
-
-    def filtered_hours(filter = :weekly, options = {})
-      Status.filtered_hours proxy_owner.id, filter, options
-    end
     
-    def hours(filter = :weekly, options = {})
-      Status.hours proxy_owner.id, filter, options
-    end
-  end
-  
   has_many :owned_projects, :order => 'projects.name', :class_name => 'Project'
 
   has_many :memberships, :dependent => :delete_all
@@ -34,10 +15,6 @@ class User < ActiveRecord::Base
   end
 
   has_finder :all, :order => 'login'
-  
-  def post(message, source = 'the web')
-    statuses.create :code_and_message => message, :source => source
-  end
   
   def related_users
     @related_users ||= with_memberships { User.find :all, :order => 'last_status_at desc', :select => "DISTINCT users.*" }
@@ -64,13 +41,7 @@ protected
       yield
     end
   end
-  
-  def can_access_status?(status)
-    status.project_id.nil? ||
-      status.user_id == id ||
-      accessible_project_id?(status.project_id)
-  end
-  
+    
   def can_access_user?(user)
     user == self || user.last_status_project_id.nil? || accessible_project_id?(user.last_status_project_id)
   end
