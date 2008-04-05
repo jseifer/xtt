8d42c51959987058e974ccfb9ea7b980db5e5003
Matt Lyon <matt@flowerpowered.com>
2008-04-04 23:18:06 -0700

    context is no longer validatable due to the uniqueness of its attributes kthx

--- app/models/context.rb
+++ app/models/context.rb
@@ -2,7 +2,9 @@ class Context < ActiveRecord::Base
   has_many :memberships
   belongs_to :user
   
-  def after_save
+  validates_uniqueness_of :name, :scope => :user_id
+  
+  def after_create
     unless user_id
       update_attribute :user_id, memberships[0].user_id unless memberships.empty?
     end
