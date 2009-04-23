6b1d0d86bd1981531919eb242995e6d89cda669f
c3 <courtenay@entp.com>
2009-04-23 16:01:56 -0700

    Move away from BJ and reimplement as DJ

--- app/models/user/inviter.rb
+++ app/models/user/inviter.rb
@@ -17,10 +17,10 @@ class User::Inviter
     ActiveRecord::Base.transaction do
       users.each do |user|
         @project.users << user
-        User::Mailer.deliver_project_invitation @project, user
+        Job::UserInviter.create @project, user
       end
       invitations.each do |invite|
-        User::Mailer.deliver_new_invitation @project, invite
+        Job::UserInviter.create @project, invite
       end
     end
   end
