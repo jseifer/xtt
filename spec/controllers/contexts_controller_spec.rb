65b62cea38ba354e64e58c82b94aee4369604ec7
c3 <courtenay@entp.com>
2009-05-15 04:29:12 -0700

    Make a note (and cleanup) for current_user in context listing.

--- spec/controllers/contexts_controller_spec.rb
+++ spec/controllers/contexts_controller_spec.rb
@@ -22,14 +22,14 @@ describe ContextsController, "GET #show" do
     controller.stub!(:current_user).and_return(@user)
   end
 
-  [ {:user_id => nil,   :filter => nil, :args => [nil, :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => 'all', :filter => nil, :args => [nil, :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => 'me',  :filter => nil, :args => [55,  :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => '5',   :filter => nil, :args => [5,   :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => nil,   :filter => 'weekly', :args => [nil, 'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => 'all', :filter => 'weekly', :args => [nil, 'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => 'me',  :filter => 'weekly', :args => [55,  'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20}]},
-    {:user_id => '5',   :filter => 'weekly', :args => [5,   'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20}]} ].each do |options|
+  [ {:user_id => nil,   :filter => nil, :args => [nil, :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => 'all', :filter => nil, :args => [nil, :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => 'me',  :filter => nil, :args => [55,  :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => '5',   :filter => nil, :args => [5,   :weekly, {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => nil,   :filter => 'weekly', :args => [nil, 'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => 'all', :filter => 'weekly', :args => [nil, 'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => 'me',  :filter => 'weekly', :args => [55,  'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]},
+    {:user_id => '5',   :filter => 'weekly', :args => [5,   'weekly', {:date => nil, :context => 'c', :page => nil, :per_page => 20, :current_user => 55}]} ].each do |options|
       
     describe ContextsController, "(filtered)" do
       define_models :contexts
@@ -39,12 +39,12 @@ describe ContextsController, "GET #show" do
       before(:each) do
         Status.should_receive(:filter).with(*options[:args]).and_return([@statuses, @date_range])
         Status.should_receive(:filter_all_users).and_return([@statuses, @date_range])
-        Status.should_receive(:filtered_hours).with(*options[:args][0..-3] + [:daily, {:date => nil, :context => 'c'}]).and_return(@hours)
-        Status.should_receive(:filtered_hours).with(*options[:args][0..-2] + [{:date => nil, :context => 'c'}]).and_return(@hours)
+        Status.should_receive(:filtered_hours).with(*options[:args][0..-3] + [:daily, {:date => nil, :context => 'c', :current_user => 55}]).and_return(@hours)
+        Status.should_receive(:filtered_hours).with(*options[:args][0..-2] + [{:date => nil, :context => 'c', :current_user => 55}]).and_return(@hours)
       end
       
       it_assigns :statuses, :date_range, :hours
-      it_renders :template, :show, :pending => true
+      it_renders :template, :show, :pending => true # Don't integrate views yet -- we're using "real" contexts
 
       describe ContextsController, "(xml)" do
         define_models :contexts
