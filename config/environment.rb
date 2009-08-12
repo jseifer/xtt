7a1248b9794b7a625d28733dc02303464206bdd8
c3 <courtenay@entp.com>
2009-08-12 15:17:36 -0700

    Upgrade to rails 2.3.3

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_tt_session',
-    :secret      => 'SEKRIT'
+    :secret      => 'SEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRITSEKRIT!'
   }
 
   config.gem 'tinder', :version => '1.2.0'
