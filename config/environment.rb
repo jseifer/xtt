23838c07cb73616a53e86532c1c6c01c3c0ec06c
c3 <courtenay@entp.com>
2009-11-05 16:59:31 -0800

    Upgrade to latest rail!

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -39,6 +39,8 @@ Rails::Initializer.run do |config|
   # rails 2.3
   # config.active_record.default_time_zone = "UTC"
   config.time_zone = "UTC"
+  # config.active_record.time_zone = "UTC"
+  config.active_record.default_timezone = :utc
   
   config.after_initialize do
     %w(ostruct md5).each { |lib| require lib }
