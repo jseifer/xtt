4caa3d59d9804c9819328b95da26eb2d9d881509
Mike Skalnik <mike.skalnik@gmail.com>
2010-06-19 02:07:04 +0800

    Added hoptoad

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,10 @@ set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage' rescue 'YOU NEED TO INSTALL THE capistrano-ext GEM'
 require 'capistrano/gitflow' rescue 'YOU NEED TO INSTALL THE capistrano-gitflow GEM'
+
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
